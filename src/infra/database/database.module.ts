d81ebba18579aa1e9db2ccb33009819bda762ea9
Kaue Alves <kaue_alves00@yahoo.com>
2024-03-01 10:48:08 -0300

    feat: Implementing Prisma repositories

---
+++ src/infra/database/database.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common'
+
+import { PrismaService } from './prisma/prisma.service'
+import { PrismaAnswersRepository } from './prisma/repositories/prisma-answers-repository'
+import { PrismaAnswerCommentsRepository } from './prisma/repositories/prisma-answer-comments-repository'
+import { PrismaAnswerAttachmentsRepository } from './prisma/repositories/prisma-answer-attachments-repository'
+
+import { PrismaQuestionsRepository } from './prisma/repositories/prisma-questions-repository'
+import { PrismaQuestionCommentsRepository } from './prisma/repositories/prisma-question-comments-repository'
+import { PrismaQuestionAttachmentsRepository } from './prisma/repositories/prisma-question-attachments-repository'
+
+@Module({
+  providers: [
+    PrismaService,
+    PrismaAnswersRepository,
+    PrismaAnswerCommentsRepository,
+    PrismaAnswerAttachmentsRepository,
+    PrismaQuestionsRepository,
+    PrismaQuestionCommentsRepository,
+    PrismaQuestionAttachmentsRepository,
+  ],
+  exports: [
+    PrismaService,
+    PrismaAnswersRepository,
+    PrismaAnswerCommentsRepository,
+    PrismaAnswerAttachmentsRepository,
+    PrismaQuestionsRepository,
+    PrismaQuestionCommentsRepository,
+    PrismaQuestionAttachmentsRepository,
+  ],
+})
+export class DatabaseModule {}
