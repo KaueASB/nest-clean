08ff4b6f736cd7aabcb77a7759bcbfe8bae22f4e
Kaue Alves <kaue_alves00@yahoo.com>
2024-03-01 11:16:13 -0300

    feat: Conversion between layers (Mappers)

---
+++ src/infra/database/prisma/mappers/prisma-question-mapper.ts
@@ -0,0 +1,22 @@
+import { Question as PrismaQuestion } from '@prisma/client'
+
+import { Question } from '@/domain/forum/enterprise/entities/question'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
+
+export class PrismaQuestionMapper {
+  static toDomain(question: PrismaQuestion): Question {
+    return Question.create(
+      {
+        title: question.title,
+        content: question.content,
+        slug: Slug.create(question.slug),
+        authorId: new UniqueEntityID(question.authorId),
+        bestAnswerId: undefined,
+        createdAt: question.createdAt,
+        updatedAt: question.updatedAt,
+      },
+      new UniqueEntityID(question.id),
+    )
+  }
+}
