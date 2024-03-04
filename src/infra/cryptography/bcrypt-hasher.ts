e2d9b1661c79d4198fe05e2fbf5ac1a2c8abe5c1
Kaue Alves <kaue_alves00@yahoo.com>
2024-03-04 10:40:54 -0300

    feat: encryption implementation

---
+++ src/infra/cryptography/bcrypt-hasher.ts
@@ -0,0 +1,14 @@
+import { HashComparer } from '@/domain/forum/application/cryptography/hash-comparer'
+import { HashGenerator } from '@/domain/forum/application/cryptography/hash-generator'
+
+import { compare, hash } from 'bcrypt'
+
+export class BcryptHasher implements HashGenerator, HashComparer {
+  async hash(plain: string): Promise<string> {
+    return await hash(plain, 8)
+  }
+
+  async compare(plain: string, hash: string): Promise<boolean> {
+    return await compare(plain, hash)
+  }
+}
